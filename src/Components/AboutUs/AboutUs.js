f5166bf31ea09531025fca259018f5871aa8e626
RazuAhmad <abdurrahmanrazu2000@gmail.com>
2023-04-08 20:15:41 +0600

    footer completed book collection starting

--- src/Components/AboutUs/AboutUs.js
+++ src/Components/AboutUs/AboutUs.js
@@ -4,77 +4,91 @@ import "./AboutUs.css";
 import Typewriter from "typewriter-effect";
 const AboutUs = () => {
   return (
-    <div className=" text-white footer_container text-xl">
+    <div className="p-6 md:px-18 md:pt-7 mt-14 md:mt-20 flex flex-col items-center justify-between text-white footer_container text-xl">
+      
+      {/* Request early access */}
       <div className="flex flex-col items-center justify-center py-12">
-        <h1 className="gradient__text">
+        <h1 className="">
           <Typewriter
             options={{
-              strings: ["Do you want to step", "into the future?"],
+              strings: ["Do you want to pre-order", "Our Latest books?"],
               autoStart: true,
               loop: true,
             }}
           />
         </h1>
-        <p>Request Early Access</p>
+        <p>Request For Pre-booking</p>
       </div>
 
      
       <div className="flex flex-col items-center">
-            
-          <div className="flex justify-between">
-          <div className=" w-1/3">
-            <h1 class="text-light">About us</h1>
-            <p class="text-muted">
-              Lorem ipsum dolar submit amet, consectetur adipisicing elit. Earum
-              maxime ea similique illum corrupti
-            </p>
-            
-          </div>
-          <div className="">
-            <h2 className="">Newsletter</h2>
-            <p className="">Stay Updated</p>
-            <form class="form-inline">
-              <di class="col pl">
-                <div class="input-group pr-5">
-                  <input 
-                    type="text"
-                    className="text-white bg-slate-400"
-                    placeholder="Email"
-                  />
-                  <div class="input-group-prepend">
-                    <div class="input-group-text">
-                      <i class="fas fa-arrow-right"></i>
-                    </div>
-                  </div>
-                </div>
-              </di>
-            </form>
-          </div>
-          <div class="col-md-2 col-sm-12">
-            <h4 class="text-light">Follow Us</h4>
-            <p class="text-muted">Let us connect ourself</p>
-            <div class="flex justify-between">
-              <i class="fab fa-facebook-f"></i>
-              <i class="fab fa-instagram"></i>
-              <i class="fab fa-twitter"></i>
-              <i class="fab fa-youtube"></i>
-            </div>
-          </div>
-          </div>
-      
 
-          <div>
-          <p class="text-sm">
-              Copyright ©2022 All rights reserved | This template is made with
-              by <span color="">Abdur Rahman</span>
-            </p>
-          </div>
+      {/* About us, Newsletter , Follow Us container */}
+      <div className="flex flex-col items-center justify-between gap-11 md:flex-row">
+            
+            {/* About us start */}
+             <div className="flex flex-col items-start w-1/3 ">
+               <h1 className="text-white font-bold text-3xl mb-3">About us</h1>
+               <p className="">
+                 Lorem ipsum dolar submit amet, consectetur adipisicing elit. Earum
+                 maxime ea similique
+               </p>
+               
+             </div>
+             {/* About us ended */}
+   
+   {/* Newsletter Section  start*/}
+   
+   <div className="flex flex-col items-start w-1/3 ">
+               <h2 className="text-white font-bold text-3xl mb-3">Newsletter</h2>
+               <p className="">Stay Updated</p>
+               
+                 <div className="flex">
+                   <p className="mt-2" >
+                     <input 
+                       type="text"
+                       className="placeholder-black "
+                       placeholder="Email"
+                     />
+                     </p>
+                     <p className="mt-4 ">
+                         <i className="text-red-60 fas fa-arrow-right cursor-pointer "></i>
+                       </p>
+                   </div>
+                 </div>
+   
+   {/* Newsletter section ended */}
+             
+             
+   
+             {/* Follow us section */}
+             <div className="flex flex-col items-start w-1/3 ">
+               <h4 className="text-white font-bold text-3xl mb-3">Follow Us</h4>
+               <p className="text-muted">Let us connect ourself</p>
+               <div className="flex justify-between mt-2">
+                 <i className="mr-2 cursor-pointer fab fa-facebook-f"></i>
+                 <i className="mr-2 cursor-pointer fab fa-instagram"></i>
+                 <i className="mr-2 cursor-pointer fab fa-twitter"></i>
+                 <i className="cursor-pointer fab fa-youtube"></i>
+               </div>
+             </div>
+   
+         </div>
+             
+      {/* About us, Newsletter, Follow us, Container  ended*/}
+
+          
           
         </div>
+        <div className="mt-24">
+          <p className="text-sm ">
+              Copyright ©2022 All rights reserved | This template is made with
+              by <span className="text-pink-300 cursor-pointer">Weero Digital Library</span>
+            </p>
+          </div>
+    </div>
       
     
-      
-    </div>
   );
 };
 
